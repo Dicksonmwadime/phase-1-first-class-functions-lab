9375285ea156db09b10ac95f7877a8a1945d38f3
Your User Name <dicksonmwadime2020@gmail.com>
2023-04-02 15:12:59 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(-2)
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers(drivers),
+    returnLastTwoDrivers(drivers)
+]
+// selectingDrivers[0](drivers)
+// selectingDrivers[1](drivers)
+
+const createFareMultiplier = (num1) => {
+    
+    return function(num2){
+        return num1 * num2
+    };
+}
+
+const fareQuintupler = createFareMultiplier(5);
+
+fareQuintupler(5)
+
+const fareDoubler = createFareMultiplier(2);
+
+fareDoubler(10)
+
+const fareTripler = createFareMultiplier(3);
+fareTripler(12)
+
+const selectDifferentDrivers = (arrayOfDrivers, func) => {
+    return func(arrayOfDrivers)
+}
+
+selectDifferentDrivers(drivers, returnFirstTwoDrivers)
+selectDifferentDrivers(drivers, returnLastTwoDrivers)
